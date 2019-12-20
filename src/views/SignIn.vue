dd9a9a2d2ea97f96075e5f74728b9574ed8668bd
EhfazAdnan <ehfaz15-167@diu.edu.bd>
2019-12-21 02:51:13 +0600

    Sending client request to api @5

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,15 +1,46 @@
 <template>
   <div>
-    Sign In
+    <form @submit.prevent="submit">
+
+      <div>
+        <label for="email">Email</label>
+        <input type="text" name="email" v-model="form.email">  
+      </div>
+
+      <div>
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" v-model="form.password">
+      </div>
+
+      <div>
+        <button type="submit">SignIn</button>
+      </div>
+
+    </form>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 
 export default {
   name: 'signin',
   components: {
-    //
+    
+  },
+  data(){
+    return{
+      form:{
+        email: '',
+        password: '',
+      }
+    }
+  },
+  methods: {
+    async submit(){
+      let response = axios.post('auth/signin', this.form)
+      console.log(response.data);
+    }
   }
 }
 </script>
