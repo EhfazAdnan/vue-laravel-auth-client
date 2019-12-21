f764cd1e300d97a6759adfafa20ea58f3e0fb6fa
EhfazAdnan <ehfaz15-167@diu.edu.bd>
2019-12-22 00:35:34 +0600

    preventing flickering 10

--- src/main.js
+++ src/main.js
@@ -10,10 +10,10 @@ axios.defaults.baseURL = 'http://127.0.0.1:8000/api'
 
 Vue.config.productionTip = false
 
-store.dispatch('auth/attempt', localStorage.getItem('token'))
-
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+store.dispatch('auth/attempt', localStorage.getItem('token')).then(() => {
+  new Vue({
+    router,
+    store,
+    render: h => h(App)
+  }).$mount('#app')
+})
